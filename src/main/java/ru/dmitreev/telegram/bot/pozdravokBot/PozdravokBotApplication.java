be0a846558e916b4a989d54a86e84481c1a7b663
Dmitree <color.ivanov2016@yandex.ru>
2023-04-10 10:24:09 +0300

    first commit

---
+++ src/main/java/ru/dmitreev/telegram/bot/pozdravokBot/PozdravokBotApplication.java
@@ -0,0 +1,13 @@
+package ru.dmitreev.telegram.bot.pozdravokBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PozdravokBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PozdravokBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/dmitreev/telegram/bot/pozdravokBot/PozdravokBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.dmitreev.telegram.bot.pozdravokBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PozdravokBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
